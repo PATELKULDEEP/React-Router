cbc3f71e40a4e58bfa051974f9d4814cbd02157f
KULDEEP PATEL <patelkuldeep0001@gmail.com>
2022-08-07 13:02:42 +0530

    Router setup done

---
+++ src/pages/BookList.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export function BookList(){
+    return (
+        <>
+        
+        <h1>Book List</h1>
+        <Link to="/books/1" >Book 1</Link>
+        <Link to="/books/2" >Book2</Link>
+        </>
+
+    );
+}
